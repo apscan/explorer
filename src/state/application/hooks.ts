b001791904f9fcf9fdba024976ed3b8aad800a19
yoshiyuki <brother-zhou@qq.com>
2022-11-04 17:12:02 +0800

    feat: sync url search params with Pagination

--- src/state/application/hooks.ts
+++ src/state/application/hooks.ts
@@ -1,9 +1,11 @@
-import { useCallback, useMemo } from 'react'
+import { useCallback, useMemo, useState } from 'react'
 import { AppState } from 'state'
 import { useAppDispatch, useAppSelector } from 'state/hooks'
 import { pageSizeSelector } from './selectors'
+import * as queryString from 'query-string'
 
 import { ApplicationModal, setOpenModal, setPageSize } from './slice'
+import { useLocation } from 'react-router-dom'
 
 export const useModalOpen = (modal: ApplicationModal): boolean => {
   const openModal = useAppSelector((state: AppState) => state.application.openModal)
@@ -29,4 +31,10 @@ export const usePageSize = () => {
   return useMemo(() => {
     return [pageSize, set] as const
   }, [pageSize, set])
+}
+
+export const usePageNumberFromUrl = (): number => {
+  const location = useLocation()
+  const [page] = useState(parseInt((queryString.parse(location.search)?.page as string) ?? '1'))
+  return page
 }
