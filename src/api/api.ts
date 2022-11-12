1ba1b9c9a4df8de0709c0b4e758125d39506b34b
Iancia <103492599+Iancia@users.noreply.github.com>
2022-11-12 17:23:48 +0800

    feat: use api host from env (#26)

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-const BASE_URL = 'https://api.apscan.io/'
+const BASE_URL = process.env.REACT_APP_API_HOST || 'https://api.apscan.io/'
 
 export const emptySplitApi = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: BASE_URL }),
