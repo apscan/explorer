740db4fafaebac52cd5863bd629ccce426795a36
FreePoix <17864213831@163.com>
2022-11-13 07:19:33 +0800

    feat: add toast component

---
+++ src/state/toast/toastSlice.ts
@@ -0,0 +1,82 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const MAX_TOASTS = 6
+let count = 0
+
+export type Toast = {
+  id?: number
+  type: string
+  message: string
+  sticky?: boolean
+}
+
+type ToastState = {
+  readonly toasts: Toast[]
+}
+
+const initialState: ToastState = {
+  toasts: [],
+}
+
+const toastSlice = createSlice({
+  name: 'toast',
+  initialState,
+  reducers: {
+    addToast(state, { payload }) {
+      const toastId = count++
+      if (state.toasts.length >= MAX_TOASTS) {
+        const i = state.toasts.findIndex((item: any) => !item.sticky)
+        state.toasts.splice(i, 1)
+      }
+      state.toasts.push({ id: toastId, ...payload })
+    },
+    removeToast(state, { payload }) {
+      state.toasts = state.toasts.filter((item: any) => item.id !== payload)
+    },
+    updateToast(state, { payload }) {
+      state.toasts = state.toasts.map((t) => {
+        if (t.id === payload.id) {
+          return { ...t, ...payload }
+        }
+        return t
+      })
+    },
+  },
+})
+
+export const toastsSelector = (state: any) => {
+  return state.toast.toasts
+}
+
+export const { addToast, removeToast, updateToast } = toastSlice.actions
+
+export const newToastId = () => count++
+
+export const newPendingToast = (id: number, message: string) =>
+  addToast({
+    id,
+    type: 'pending',
+    message,
+    sticky: true,
+  })
+
+export const updatePendingToast = (id: number, message: string) =>
+  updateToast({
+    id,
+    message,
+  })
+
+export const newSuccessToast = (message: string) =>
+  addToast({
+    type: 'success',
+    message,
+  })
+
+export const newErrorToast = (message: string) => {
+  return addToast({
+    type: 'error',
+    message,
+  })
+}
+
+export default toastSlice.reducer
