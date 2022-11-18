08c620a3c7db0161c94077a4ea00eca97391f52a
Iancia <103492599+Iancia@users.noreply.github.com>
2022-11-18 14:22:31 +0800

    Feat/one toast reload (#58)
    * feat: one toast until reload for access limit
    
    * fix: interval 30000
    
    Co-authored-by: FreePoix <17864213831@163.com>

--- src/components/toast/index.tsx
+++ src/components/toast/index.tsx
@@ -1,9 +1,8 @@
-import React, { memo, useEffect } from 'react'
+import React, { memo } from 'react'
 import styled from 'styled-components'
 import ToastItem from './ToastItem'
-import { newErrorToast, removeAllToast, toastsSelector } from '../../state/toast/toastSlice'
+import { toastsSelector } from '../../state/toast/toastSlice'
 import { useAppSelector } from '../../state/hooks'
-import { useDispatch } from 'react-redux'
 
 const Flex = styled.div`
   display: flex;
@@ -14,18 +13,26 @@ const Wrapper = styled.div`
   position: fixed;
   width: 100vw;
   height: 0;
-  left: 0;
+  right: 100px;
   top: 0;
   z-index: 999;
 `
 
 const ToastList = styled(Flex)`
   flex-direction: column-reverse;
-  margin: 78px -53px auto auto;
+  margin: 78px -240px auto auto;
   width: fit-content;
 
   > :not(:last-child) {
     margin-top: 16px;
+  }
+
+  @media screen and (max-width: 1400px) {
+    margin: 78px -180px auto auto;
+  }
+
+  @media screen and (max-width: 1300px) {
+    margin: 78px -80px auto auto;
   }
 
   @media screen and (max-width: 375px) {
@@ -35,20 +42,6 @@ const ToastList = styled(Flex)`
 
 const Toast = memo(() => {
   const toasts = useAppSelector(toastsSelector)
-  const dispatch = useDispatch()
-
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      window.onmessage = ({ data: message }) => {
-        if (message === 'toast:error:403') {
-          dispatch(newErrorToast('Access Limited, Wait for 30 Seconds', true))
-        }
-        if (message === 'toast:error:clear') {
-          dispatch(removeAllToast())
-        }
-      }
-    }
-  }, [dispatch])
 
   return (
     <Wrapper>
