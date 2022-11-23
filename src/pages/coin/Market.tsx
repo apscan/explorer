1f78cae185aad9063651d9238297afcc3e9e79c4
Iancia <freepoix@gmail.com>
2022-11-24 07:18:36 +0800

    feat: import apt price & fix create time

--- src/pages/coin/Market.tsx
+++ src/pages/coin/Market.tsx
@@ -2,15 +2,42 @@ import { AmountFormat } from 'components/AmountFormat'
 import { Box } from 'components/container'
 import { renderRow } from 'components/helpers'
 import { Card } from 'components/Card'
+import { useMemo } from 'react'
+import { FixedNumber } from '@ethersproject/bignumber'
+import { toFixedNumber } from 'utils/number'
+import { NumberFormat } from 'components/NumberFormat'
 
-export const Market = ({ data }: { data: any }) => {
+export const Market = ({ data, price }: { data?: any; price?: string }) => {
+  data = data || {}
+  const fully = useMemo(
+    () =>
+      !price
+        ? undefined
+        : FixedNumber.from(price.toString(), 'fixed128x18').mulUnsafe(
+            toFixedNumber(data.total_supply).toFormat('fixed128x18')
+          ),
+    [data.total_supply, price]
+  )
+
   return (
-    <Card marginBottom="20px">
+    <Card>
       <Box padding="0 12px">
-        {renderRow('Price', '-')}
-        {renderRow('Total Supply', <AmountFormat value={data?.total_supply} />)}
-        {renderRow('Supply Limit', data?.supply_limit ? <AmountFormat value={data?.supply_limit} /> : '-')}
-        {renderRow('Fully Diluted Valuation', '-')}
+        {renderRow('Price', <NumberFormat maximumFractionDigits={2} prefix="$" value={price} fallback="--" />)}
+        {renderRow('Total Supply', <AmountFormat value={data.total_supply} />)}
+        {renderRow('Supply Limit', data.supply_limit ? <AmountFormat value={data.supply_limit} /> : '-')}
+        {renderRow(
+          'Fully Diluted Valuation',
+          <NumberFormat
+            textTransform="uppercase"
+            abbr
+            forceAverage="billion"
+            useGrouping
+            maximumFractionDigits={3}
+            prefix="$"
+            value={fully}
+            fallback="--"
+          />
+        )}
       </Box>
     </Card>
   )
