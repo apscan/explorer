fe6788867fe4345ef5a5ffacfa9ae512a6dc8d82
Iancia <freepoix@gmail.com>
2023-01-05 13:48:24 +0800

    fix: Mutability

--- src/components/Mutability.tsx
+++ src/components/Mutability.tsx
@@ -1,22 +1,16 @@
-import { EditIcon } from '@chakra-ui/icons'
 import { IconProps } from '@chakra-ui/icons'
 import Unlock from 'assets/icons/Unlock'
 import { memo } from 'react'
 
 export interface MutabilityProps extends IconProps {
   mutable?: boolean
-  showMutable?: boolean
 }
 
-export const Mutability = memo(({ mutable, showMutable = false, ...props }: MutabilityProps) => {
-  if (typeof mutable === 'undefined') {
-    return <>-</>
+export const Mutability = memo(({ mutable, ...props }: MutabilityProps) => {
+  if (!mutable) {
+    return <Unlock {...props} />
   }
-
-  if (mutable && showMutable) {
-    return <EditIcon {...props} />
-  }
-  return <Unlock {...props} />
+  return <></>
 })
 
 Mutability.displayName = 'Mutability'
