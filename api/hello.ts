b9f1e782432a1d4a363de7a2b4746c3a8a37e4be
Iancia <freepoix@gmail.com>
2023-01-17 21:13:09 +0800

    fix: hello types

--- api/hello.ts
+++ api/hello.ts
@@ -1,4 +1,6 @@
-export default function hello(req, res) {
+import type { VercelRequest, VercelResponse } from '@vercel/node'
+
+export default function hello(req: VercelRequest, res: VercelResponse) {
   res.statusCode = 200
   res.json({ message: 'hello works' })
 }
