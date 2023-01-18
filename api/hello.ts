c10f0b91b4ffc9f4d925b4aff9b17066cb4b4dcc
Iancia <freepoix@gmail.com>
2023-01-18 19:21:18 +0800

    fix: remove hello

--- api/hello.ts
+++
@@ -1,6 +0,0 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node'
-
-export default function hello(req: VercelRequest, res: VercelResponse) {
-  res.statusCode = 200
-  res.json({ message: 'hello works' })
-}
